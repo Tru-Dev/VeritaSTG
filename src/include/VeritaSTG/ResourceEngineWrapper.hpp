807d19cf29876cba5a22a643f14c0cc25ee3ea57
TruDev <29785998+Tru-Dev@users.noreply.github.com>
2022-05-31 20:37:40 -0700

    Moved definitions to header file

--- src/include/VeritaSTG/ResourceEngineWrapper.hpp
+++ src/include/VeritaSTG/ResourceEngineWrapper.hpp
@@ -9,19 +9,41 @@
 
 #include <string>
 
+#include <fmt/core.h>
 #include <miniaudio.h>
 
 #include "Resource.hpp"
+#include "engine_panic.hpp"
+#include "log.hpp"
 
 namespace VeritaSTG {
     class ResourceEngineWrapper{
-        ma_engine miniaudio_bgm;
-        // ma_engine miniaudio_sfx;
+        ma_engine miniaudio_inst;
+
+        bool initialized = false;
     public:
-        inline ResourceResult<ma_sound> LoadMusic(std::string path) {
+        ~ResourceEngineWrapper() {
+            if (initialized) Uninitialize();
+        }
+
+        void Initialize(void) {
+            if (initialized) return;
+            ma_result result = ma_engine_init(NULL, &miniaudio_inst);
+            if (result != 0) 
+                EnginePanic(fmt::format("Could not load the audio engine: {}", result));
+            initialized = true;
+        }
+
+        void Uninitialize(void) {
+            if (!initialized) return;
+            ma_engine_uninit(&miniaudio_inst);
+            initialized = false;
+        }
+
+        ResourceResult<ma_sound> LoadMusic(std::string path) {
             ma_sound bgm;
             ma_result result = ma_sound_init_from_file(
-                &miniaudio_bgm,
+                &miniaudio_inst,
                 path.c_str(),
                 MA_SOUND_FLAG_STREAM | MA_SOUND_FLAG_NO_SPATIALIZATION,
                 NULL,
